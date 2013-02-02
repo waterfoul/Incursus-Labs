f32a419d65c59b61d5266401c9c1569d54734f38
Aaron Aichlmayr <waterfoul@gmail.com>
2013-02-02 12:21:45 -0600

    I derped

--- cronStart.php
+++ cronStart.php
@@ -2,7 +2,7 @@
 	include("config.php");
 	include("apiSync/APIkey.php");
 	include("apiSync/syncPermissions.php");
-	syncKeys($mysql_host, $mysql_phpBB_db, $mysql_phpBB_prefix, $mysql_user, $mysql_password);
+	syncKeys($mysql_host, $mysql_yapeal_db, $mysql_phpBB_db, $mysql_phpBB_prefix, $mysql_user, $mysql_password);
 	//exec("cd yapeal && php yapeal.php");
-	sync_all_permissions($mysql_host, $mysql_yapeal_db, $mysql_phpBB_db, $mysql_phpBB_prefix, $mysql_user, $mysql_password);
+	sync_all_permissions($mysql_host, $mysql_phpBB_db, $mysql_phpBB_prefix, $mysql_user, $mysql_password);
 ?>
