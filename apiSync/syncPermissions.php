9cbd0c60c99ecf63ec7f2a71fc440ca15d293db1
Aaron Aichlmayr <waterfoul@gmail.com>
2013-02-07 01:21:30 -0600

    added mail checking, minor refactor

--- apiSync/syncPermissions.php
+++ apiSync/syncPermissions.php
@@ -119,7 +119,7 @@
 				))
 					if(($mask & 90179600)==90179600 && $blocklevel < 2 && isSendingIsk($charIds, $yapeal, $block[3]) === false && $row->pf_api_key_lowsec == 1)
 					{
-						mailNotify($charIds);
+						mailNotify($charIds, $row->username, $yapeal, $block[3]);
 						if(($mask & 393226)==393226 && $blocklevel < 1 && $row->pf_api_key_corp == 1)
 						{
 							/*TODO:Add corp check*/
@@ -199,7 +199,7 @@
 				LEFT JOIN " . $mysql_eve_dbDump . ".invTypes as t ON i.typeID = t.typeID
 			WHERE
 				a.characterID = " . $char . "
-				AND a.verifiedOK = 0
+				AND a.verifiedOK != 1
 				AND s.security > 0.4
 				AND t.groupID NOT IN (28, 29, 31, 237, 380, 463, 513, 543, 902, 941)" //Industrial, Capsule, Shuttle, Rookie ship, Transport Ship, Mining Barge, Freighter, Exhumer, Jump Freighter, Industrial Command Ship 
 			);
@@ -217,7 +217,7 @@
 				LEFT JOIN " . $mysql_eve_dbDump . ".invTypes as t ON i.typeID = t.typeID
 			WHERE
 				a.characterID = " . $char . "
-				AND a.verifiedOK = 0
+				AND a.verifiedOK != 1
 				AND s.security > 0.4
 				AND t.groupID IN (28, 380, 513, 902, 941)" //Industrial, Transport Ship, Freighter, Jump Freighter, Industrial Command Ship 
 			);
@@ -245,108 +245,173 @@
 	}
 	function isSendingIsk($characterIds, $yapeal, $blocklist)
 	{
-		$utc = new DateTimeZone("UTC");
 		foreach($characterIds as $char)
 		{
 			$qry = $yapeal->query(
 				"SELECT w.`refID`, w.`ownerID2`, w.`ownerName2` FROM
 	    			`charWalletJournal` as w 
 				 WHERE
-					w.`verifiedOK` = 0
+					w.`verifiedOK` != 1
 	     			AND w.`refTypeID` IN (0, 1, 6, 10, 35, 37, 71)
 	      			AND w.`ownerID1` = " . $char
 			);
 			$char = true;
 			while($row = $qry->fetch_object())
 			{
-				$qry2 = $yapeal->query(
-					"SELECT i.`characterName`, i.`corporation`, i.`alliance`, i.`cachedUntil` FROM
-		    			`custom_characterInfo` as i
-					 WHERE
-						characterID = " . $row->ownerID2
-				);
-				$row2 = null;
-				if(!($row2 = $qry2->fetch_object()) || DateTime::createFromFormat("Y-m-d H:i:s", $row2->cachedUntil, $utc) < new DateTime())
-				{
-					$xml = simplexml_load_file("https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=" . $row->ownerID2);
-					if(!empty($xml->error))
-						continue;
-					$yapeal->query(
-						"INSERT INTO `Incursus_yapeal`.`custom_characterInfo`
-						(`characterID`, `characterName`, `race`, `bloodline`, `corporationID`, `corporation`,
-							`corporationDate`, `allianceID`, `alliance`, `allianceDate`, `securityStatus`, `cachedUntil`) VALUES
-						('" . $xml->result->characterID . "', '" . $xml->result->characterName . "', '" . $xml->result->race . "', '" . $xml->result->bloodline . "', '" . $xml->result->corporationID . "', '" . $xml->result->corporation . "',
-							'" . $xml->result->corporationDate . "', '" . $xml->result->allianceID . "', '" . $xml->result->alliance . "', '" . $xml->result->allianceDate . "', '" . $xml->result->securityStatus . "', '" . $xml->cachedUntil . "');"
-					);
-					
-					$qry2 = $yapeal->query(
-						"SELECT i.`characterName`, i.`corporation`, i.`alliance`, i.`cachedUntil` FROM
-			    			`custom_characterInfo` as i
-						 WHERE
-							characterID = " . $row->ownerID2
-					);
-					$row2 = $qry2->fetch_object();
-					if(!$row2)
-						$char = false;
-				}
-				if(!$char || strtolower($row2->characterName) != strtolower($row->ownerName2))
-				{
-					$qry3 = $yapeal->query(
-						"SELECT * FROM
-			    			`custom_corpInfo` as i
-						 WHERE
-							corpID = " . $row->ownerID2
-					);
-					$row3 = null;
-					if(!($row3 = $qry3->fetch_object()) || DateTime::createFromFormat("Y-m-d H:i:s", $row3->cachedUntil, DateTimeZone::UTC) < new DateTime())
-					{
-						$xml = simplexml_load_file("https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=" . $row->ownerID2);
-						if(!empty($xml->error))
-							continue;
-						$yapeal->query(
-							"INSERT INTO `Incursus_yapeal`.`custom_corpInfo`
-							(`corpID`, `corpName`, `cachedUntil`) VALUES
-							('" . $xml->result->corporationID . "', '" . $xml->result->corporationName . "', '" . $xml->cachedUntil . "');"
-						);
-						
-						$qry3 = $yapeal->query(
-							"SELECT * FROM
-				    			`custom_corpInfo` as i
-							 WHERE
-								corpID = " . $row->ownerID2
-						);
-						$row3 = $qry3->fetch_object();
-						if(!$row3)
-							$corp = false;
-					}
-					if(!$corp || strtolower($row3->corpName) != strtolower($row->ownerName2))
-					{
-						if(in_array(strtolower($row->ownerName2), $blocklist["Alliance"]))
-							return $row->refID;
-					}
-					else
-					{
-						$qry4=$yapeal->query("SELECT l.name FROM  `eveMemberCorporations` as m LEFT JOIN eveAllianceList as l ON m.allianceID = l.allianceID WHERE m.corporationID = " . $row3->corpID);
-						if(($row4 = $qry4->fetch_object() && in_array(strtolower($row4->name), $blocklist["Alliance"])) ||
-							in_array(strtolower($row3->corpID), $blocklist["Corporation"])
-							
-						)
-							return $row->refID;
-					}
-					
-				}
-				else if(
-					in_array(strtolower($row2->characterName), $blocklist["Character"]) ||
-					in_array(strtolower($row2->corporation), $blocklist["Corporation"]) ||
-					in_array(strtolower($row2->alliance), $blocklist["Alliance"])
-				)
+				if(id_on_blocklist($row->ownerID2, $row->ownerName2, $yapeal, $blocklist))
 					return $row->refID;
+				$yapeal->query("UPDATE charWalletJournal SET verifiedOK = 1 WHERE refID = " . $row->refID);
 			}
 		}
 		return false;
 	}
-	function mailNotify($characterIds)
+	function mailNotify($characterIds, $name, $yapeal, $blocklist)
 	{
-		
+		foreach($characterIds as $char)
+		{
+		$utc = new DateTimeZone("UTC");
+			$qry = $yapeal->query(
+				"SELECT m.`ownerID`, m.`messageID`, m.`title`, m.`messageID`, m.`toCharacterIDs`, m.`toCorpOrAllianceID` FROM
+	    			`charMailMessages` as m
+				 WHERE
+					m.`mailSent` != 1
+	      			AND m.`ownerID` = m.`senderID`
+	      			AND m.`ownerID` = " . $char
+			);
+			$char = true;
+			while($row = $qry->fetch_object())
+			{
+				$sendmail = false;
+				$chars = split(",", $row->toCharacterIDs);
+				foreach($chars as $c)
+				{
+					$char = get_char_from_cust($c, $utc);
+					if(	$char != null && (
+						in_array(strtolower($char->characterName), $blocklist["Character"]) ||
+						in_array(strtolower($char->corporation), $blocklist["Corporation"]) ||
+						in_array(strtolower($char->alliance), $blocklist["Alliance"])
+					))
+						$sendmail = $char->characterName . " (" . $char->corporation . "/" . $char->alliance . ")";
+				}
+				$corpOrAlliance = $row->toCorpOrAllianceID;
+				$corp = get_corp_from_cust($id, $utc);
+				if($sendmail === false && $corp != null)
+				{
+					if(in_array(strtolower($corp->corpName), $blocklist["Corporation"]))
+						$sendmail = $corp->corpName;
+					$qry4=$yapeal->query("SELECT l.name FROM `eveMemberCorporations` as m LEFT JOIN eveAllianceList as l ON m.allianceID = l.allianceID WHERE m.corporationID = " . $corp->corpID);
+					if($row4 = $qry4->fetch_object() && in_array(strtolower($row4->name), $blocklist["Alliance"]))
+						$sendmail = $corp->corpName . " (" . $row4->name . ")";
+					
+				$alliance=$yapeal->query("SELECT l.name FROM eveAllianceList as l WHERE l.allianceID = " . $id);
+				if($sendmail === false && $alliance != null && in_array(strtolower($alliance->name), $blocklist["Alliance"]))
+					$sendmail = $alliance->name;
+				if($sendmail !== false)
+				{
+					if(mail("IncursusForums@gmail.com", "Evemail Flagged", "User: " . $name . "\nTo: " . $sendmail . "\nSubject: " . $row->title))
+						$yapeal->query("UPDATE charMailMessages SET mailSent = 1 WHERE messageID = " . $row->messageID);
+				}
+				else
+					$yapeal->query("UPDATE charMailMessages SET mailSent = 1 WHERE messageID = " . $row->messageID);
+			}
+		}
+		return false;
+	}
+	
+	function id_on_blocklist($id, $name, $yapeal, $blocklist)
+	{
+		$utc = new DateTimeZone("UTC");
+		$row2 = get_char_from_cust($id, $utc);
+		if($row2 == null || strtolower($row2->characterName) != strtolower($name))
+		{
+			$row3 = get_corp_from_cust($id, $utc);
+			if($row3 == null || strtolower($row3->corpName) != strtolower($name))
+			{
+				if(in_array(strtolower($name), $blocklist["Alliance"]))
+					return true;
+			}
+			else
+			{
+				$qry4=$yapeal->query("SELECT l.name FROM `eveMemberCorporations` as m LEFT JOIN eveAllianceList as l ON m.allianceID = l.allianceID WHERE m.corporationID = " . $row3->corpID);
+				if(($row4 = $qry4->fetch_object() && in_array(strtolower($row4->name), $blocklist["Alliance"])) ||
+					in_array(strtolower($row3->corpID), $blocklist["Corporation"])
+				)
+					return true;
+			}
+			
+		}
+		else if(
+			in_array(strtolower($row2->characterName), $blocklist["Character"]) ||
+			in_array(strtolower($row2->corporation), $blocklist["Corporation"]) ||
+			in_array(strtolower($row2->alliance), $blocklist["Alliance"])
+		)
+			return true;
+		return false;
+	}
+	function get_char_from_cust($id, $utc)
+	{
+		$qry2 = $yapeal->query(
+			"SELECT i.`characterName`, i.`corporation`, i.`alliance`, i.`cachedUntil` FROM
+    			`custom_characterInfo` as i
+			 WHERE
+				characterID = " . $id
+		);
+		$row2 = null;
+		if(!($row2 = $qry2->fetch_object()) || DateTime::createFromFormat("Y-m-d H:i:s", $row2->cachedUntil, $utc) < new DateTime())
+		{
+			$xml = simplexml_load_file("https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=" . $id);
+			if(!empty($xml->error))
+				continue;
+			$yapeal->query(
+				"INSERT INTO `Incursus_yapeal`.`custom_characterInfo`
+				(`characterID`, `characterName`, `race`, `bloodline`, `corporationID`, `corporation`,
+					`corporationDate`, `allianceID`, `alliance`, `allianceDate`, `securityStatus`, `cachedUntil`) VALUES
+				('" . $xml->result->characterID . "', '" . $xml->result->characterName . "', '" . $xml->result->race . "', '" . $xml->result->bloodline . "', '" . $xml->result->corporationID . "', '" . $xml->result->corporation . "',
+					'" . $xml->result->corporationDate . "', '" . $xml->result->allianceID . "', '" . $xml->result->alliance . "', '" . $xml->result->allianceDate . "', '" . $xml->result->securityStatus . "', '" . $xml->cachedUntil . "');"
+			);
+			
+			$qry2 = $yapeal->query(
+				"SELECT i.`characterName`, i.`corporation`, i.`alliance`, i.`cachedUntil` FROM
+	    			`custom_characterInfo` as i
+				 WHERE
+					characterID = " . $id
+			);
+			$row2 = $qry2->fetch_object();
+			if(!$row2)
+				return null;
+		}
+		return $row2;
+	}
+	function get_corp_from_cust($id, $utc)
+	{
+		$qry3 = $yapeal->query(
+			"SELECT * FROM
+    			`custom_corpInfo` as i
+			 WHERE
+				corpID = " . $id
+		);
+		$row3 = null;
+		if(!($row3 = $qry3->fetch_object()) || DateTime::createFromFormat("Y-m-d H:i:s", $row3->cachedUntil, DateTimeZone::UTC) < new DateTime())
+		{
+			$xml = simplexml_load_file("https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=" . $id);
+			if(!empty($xml->error))
+				continue;
+			$yapeal->query(
+				"INSERT INTO `Incursus_yapeal`.`custom_corpInfo`
+				(`corpID`, `corpName`, `cachedUntil`) VALUES
+				('" . $xml->result->corporationID . "', '" . $xml->result->corporationName . "', '" . $xml->cachedUntil . "');"
+			);
+			
+			$qry3 = $yapeal->query(
+				"SELECT * FROM
+	    			`custom_corpInfo` as i
+				 WHERE
+					corpID = " . $id
+			);
+			$row3 = $qry3->fetch_object();
+			if(!$row3)
+				return null;
+		}
+		return $row3;
 	}
 ?>
