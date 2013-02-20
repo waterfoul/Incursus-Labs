b9b7e77faeb90479eda2e9bfb498b1c2cd4ca10b
waterfoul <root@localhost.localdomain>
2013-02-20 12:58:19 -0600

    Fixed the poker

--- ts3PHP/clientUsernameCheck.php
+++ ts3PHP/clientUsernameCheck.php
@@ -1,6 +1,7 @@
 <?php
+	require_once("TeamSpeak3/TeamSpeak3.php");
 	/* connect to server, login and get TeamSpeak3_Node_Host object by URI */
-	$ts3_ServerInstance = TeamSpeak3::factory("serverquery://phpUser:OJRuXDTg@localhost/");
+	$ts3_ServerInstance = TeamSpeak3::factory("serverquery://serveradmin:aVMT3VfwUxSpME8x9DLehHBK@localhost:10011/");
 	
 	/* display server select form */
 	$servers = $ts3_ServerInstance->serverList();
@@ -8,5 +9,7 @@
 	foreach($servers as $v)
 	  	foreach($v->clientList() as $client)
   		{
-  			$client->poke("test");
+			try{
+				$client->poke("test");
+			}catch(TeamSpeak3_Adapter_ServerQuery_Exception $e){};
 		}
