9a399f6f0613d24b57543682fdc5af3c03353c8f
Aaron Aichlmayr <waterfoul@gmail.com>
2013-05-19 23:04:17 -0500

    fixed the db query

--- industry/index.php
+++ industry/index.php
@@ -17,33 +17,27 @@
 					exit();
 				$db = new mysqli($mysql_host, $mysql_evecentral_username, $mysql_evecentral_password, $mysql_evecentral);
 				$qry = $db->query("
-					SELECT DISTINCT(c.`itemID`), g.`groupName`, c.`Profit`, c.`Date`, i.`typeName`
+					SELECT DISTINCT(c.`itemID`), g.`groupName`, c.`Profit`, c.`Date`, i.`typeName`, b.`researchTechTime`, b.`productionTime`, b.`researchCopyTime`, d.`valueInt`, d.`valueFloat`
 					FROM  `calculatedTheory` c
-					LEFT JOIN  `naa_dbdump`.`invTypes` AS i ON c.`itemID` = i.`typeID`
-					LEFT JOIN  `naa_dbdump`.`invGroups` AS g ON i.`groupID` = g.`groupID`
+					LEFT JOIN `naa_dbdump`.`invTypes` AS i ON c.`itemID` = i.`typeID`
+					LEFT JOIN `naa_dbdump`.`invGroups` AS g ON i.`groupID` = g.`groupID`
+					LEFT JOIN `naa_dbdump`.`invBlueprintTypes` as b ON b.`productTypeID` = i.`typeID`
+					LEFT OUTER JOIN `naa_dbdump`.`dgmTypeAttributes` as d ON d.`typeID` = b.`productTypeID` AND d.`attributeID` = 422
 					ORDER BY  `c`.`Profit` DESC,
 						      `c`.`Date` DESC
 				");
 				print("<table id='maintable' class='filterable sortable'><tr><th>Item Name</th><th>Group</th><th>Profit</th><th>Build Time</th><th>Invent Time</th><th>Copy Time</th><th>Total Time</th><th>Date</th><th>Item ID</th></tr>");
 				while($row = $qry->fetch_object())
 				{
-					$q4=$dump->query("SELECT `researchTechTime`,`productionTime`,`researchCopyTime` FROM `invBlueprintTypes` WHERE `productTypeID` = " . $item[0]);
-					$buildtime = 0;
+					$buildtime = $row->productionTime/60/60;
 		            $inventtime = 0;
 		            $copytime = 0;
-		            $totaltime = 0;
-			        $r4=$q4->fetch_object();
-					if($r4){
-						$buildtime = $r4->productionTime/60/60;
-			            $inventtime = 0;
-			            $copytime = 0;
-			            if($item[1] == 2)
-			            {
-			            	$copytime = ($r4->researchCopyTime * 2)/60/60;
-			            	$inventtime = $r4->researchTechTime/60/60;
-			        	}
-			            $totaltime = $copytime/10 + $buildtime + $inventtime/10;
-		            }
+		            if($row->valueInt == 2 || $row->valueFloat == 2)
+		            {
+		            	$copytime = ($row->researchCopyTime * 2)/60/60;
+		            	$inventtime = $row->researchTechTime/60/60;
+		        	}
+		            $totaltime = $copytime/10 + $buildtime + $inventtime/10;
 					print("<tr><td>" . $row->typeName . "</td><td>" . $row->groupName . "</td><td>" . number_format($row->Profit,2) . "</td><td>" . $buildtime . "</td><td>" . $inventtime . "</td><td>" . $copytime . "</td><td>" . $totaltime . "</td><td>" . $row->Date . "</td><td>" . $row->itemID . "</td></tr>");
 				}
 				print("</table>")
